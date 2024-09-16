fbc5652cb6fbb11e1550958ea229be26cdad8592
Jeremiahtaylor2017 <jeremiahtaylor2017@gmail.com>
2024-09-15 18:46:47 -0700

    added asteroid and asteroid field instances

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from constants import (
     ASTEROID_MAX_RADIUS,
 )
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -16,12 +18,16 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    updatable = pygame.sprite.Group()  # updatable objects
-    drawable = pygame.sprite.Group()  # drawable objects
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
 
     running = True
     while running:
