178dae3c62610c07b18445e1433cc0a445eee835
Jeremiahtaylor2017 <jeremiahtaylor2017@gmail.com>
2024-09-15 20:45:43 -0700

    used split method in game loop

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main():
             for shot in shots:
                 if shot.collision(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill((0, 0, 0))
 
